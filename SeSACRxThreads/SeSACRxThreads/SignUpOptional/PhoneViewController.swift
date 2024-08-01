fd9e8495bf4229265fb55c473a0de699ee40adc6
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-08-02 00:14:44 +0900

    [Feat] BehaviorRelay, accept, bind 사용

--- SeSACRxThreads/SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -6,12 +6,26 @@
 //
  
 import UIKit
+
+import RxSwift
+import RxCocoa
 import SnapKit
+
+/*
+ 첫 진입시 010 바로 띄우기 - Behavior
+ - textField는 숫자, 10자 이상.
+ - validation : color.pink : color.lightGray
+ */
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    let descriptionLabel = UILabel()
+    
+    let numberText = BehaviorRelay(value: "010")
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,18 +33,13 @@ class PhoneViewController: UIViewController {
         view.backgroundColor = Color.white
         
         configureLayout()
-        
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
-    }
-    
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(NicknameViewController(), animated: true)
+        bindData()
     }
 
-    
     func configureLayout() {
         view.addSubview(phoneTextField)
         view.addSubview(nextButton)
+        view.addSubview(descriptionLabel)
          
         phoneTextField.snp.makeConstraints { make in
             make.height.equalTo(50)
@@ -43,6 +52,47 @@ class PhoneViewController: UIViewController {
             make.top.equalTo(phoneTextField.snp.bottom).offset(30)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(phoneTextField.snp.bottom)
+            make.bottom.equalTo(nextButton.snp.top)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+    }
+    func bindData() {
+        phoneTextField.keyboardType = .numberPad
+        
+        let validation = phoneTextField.rx.text.orEmpty
+            .map { $0.count >= 10 }
+        
+        numberText
+            .bind(with: self) { owner, number in
+                owner.phoneTextField.text = number
+            }
+            .disposed(by: disposeBag)
+        
+        phoneTextField.rx.text.orEmpty
+            .bind(with: self) { owner, number in
+                owner.numberText.accept(number)
+            }
+            .disposed(by: disposeBag)
+        
+        validation
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        validation
+            .bind(with: self) { owner, value in
+                let color: UIColor = value ? .systemPink : .lightGray
+                owner.nextButton.backgroundColor = color
+            }
+            .disposed(by: disposeBag)
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(NicknameViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
+        
     }
 
 }
